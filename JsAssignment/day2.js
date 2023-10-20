cac787537b09b69ef451a2cff25d6bd1435667ee
MOHD-MERAJ-1208 <mohdmeraj6068@gmail.com>
2023-10-20 09:21:54 +0500

    day2.js file

---
+++ JsAssignment/day2.js
@@ -0,0 +1,23 @@
+// var str ="javascript"
+// console.log(str)
+// console.log(str.length)
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
+// console.log(str.substr(4,6))
+// console.log(str.split(" "))
+// console.log(str.trim(""))
+                                                                        
+
+// var a = 41.23456
+// // var b = 52.23456
+// console.log(math.round(a))
+
+// var salary = 50000
+// var bonus = 10000
+// console.log(salary*12 + bonus)
+
+var str = "He earns 60000 from salary per month, 30000 annual bonus."
+var salary = str.substr(9,5)
+var bonus = str.substr(38,5)
+var AnnualIncome =salary*12 + Number (bonus)
+console.log('Total AnnualIncome =', AnnualIncome)
