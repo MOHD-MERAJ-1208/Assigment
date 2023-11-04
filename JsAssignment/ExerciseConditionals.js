edd5c30c2ef85cd2d03a2cdace4b9318aeadbc1f
MOHD-MERAJ-1208 <mohdmeraj6068@gmail.com>
2023-11-04 19:26:24 +0500

    updated files

--- JsAssignment/ExerciseConditionals.js
+++ JsAssignment/ExerciseConditionals.js
@@ -1,3 +1,48 @@
+//--------------------------------------------------------------------ExerciseConditionals-1--------------------------------------------------------------//
+
+// var a = 230
+// var b = 10
+// if(a>b){
+//     console.log("a is greater than b")
+// }
+// else{
+//     console.log("a is less than b")
+// }
+
+
+// var num = prompt('Enter a number:');
+// if(num%2==0){
+//     alert(num+"is an Even Number")
+// }
+// else{
+//     alert(num+"is an Odd Number")
+// }
+
+
+// var age = prompt('Enter Your Age:')
+// var max=18
+// if (age>=18) {
+//     alert("You are Old enough to Drive")
+// }
+// else{
+//     var number = max-age
+//     alert("You are left with '+ number +' Years to Drive")
+// }
+
+
+var a=3
+var b=20
+var c=54
+
+if (a>b && a>c) {
+    console.log("a is the Largest")
+}
+else{
+    
+}
+
+//--------------------------------------------------------------------ExerciseConditionals-2--------------------------------------------------------------//
+
 // var obj = {
 //     admin : ['create','update','delete'],
 //     user : ['Update','remove'],
